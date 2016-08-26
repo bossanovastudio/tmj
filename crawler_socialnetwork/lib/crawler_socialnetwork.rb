021771582f724dc56ac211f5283e32984962977f
Adão Raul <adao.raul@gmail.com>
2016-08-26 19:19:51 -0300

    social network cli skel

--- crawler_socialnetwork/lib/crawler_socialnetwork.rb
+++ crawler_socialnetwork/lib/crawler_socialnetwork.rb
@@ -1,4 +1,7 @@
 require "crawler_socialnetwork/version"
+require "crawler_socialnetwork/facebook"
+require "crawler_socialnetwork/twitter"
+require "crawler_socialnetwork/cli"
 
 module CrawlerSocialnetwork
   # Your code goes here...
---
+++ crawler_socialnetwork/lib/crawler_socialnetwork/cli.rb
@@ -0,0 +1,11 @@
+module CrawlerSocialnetwork
+  class CLI
+    def initialize
+
+    end
+
+    def run
+
+    end
+  end
+end
