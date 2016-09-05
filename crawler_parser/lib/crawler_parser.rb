8a93577f90f07fa6f6dae0e9d80be6f046928275
Adão Raul <adao.raul@gmail.com>
2016-09-05 18:08:53 -0300

    crawler_parser logger actived

--- crawler_parser/lib/crawler_parser.rb
+++ crawler_parser/lib/crawler_parser.rb
@@ -1,2 +1,15 @@
-require "crawler_parser/version"
+require "logger"
 
+require "active_record"
+require "active_resource"
+
+require "crawler_parser/version"
+require "crawler_parser/models/crawled_record"
+require "crawler_parser/models/crawled_post"
+require "crawler_parser/models/card"
+require "crawler_parser/cli"
+
+module CrawlerParser
+  $logger = Logger.new(STDOUT)
+  $logger.level = Logger::WARN
+end
