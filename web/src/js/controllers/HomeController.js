f000772b2ff40c8dbdb1ce5ea19070c0acac8404
Jonathan Querubina <jotadev@me.com>
2016-09-25 16:12:50 -0300

    Add masonry support

--- web/src/js/controllers/HomeController.js
+++ web/src/js/controllers/HomeController.js
@@ -1,3 +1,9 @@
 tmj.controller('HomeController', function($rootScope, $scope, $http, $sce) {
-
+    $('.cards').masonry({
+        itemSelector: '.card',
+        columnWidth: 100,
+        percentPosition: true,
+        gutter: 24,
+        percentPosition: true
+    });
 });
