09f0ab2fdbd82e869ecbe1c7382409c7f560cc15
Jonathan Querubina <jotadev@me.com>
2016-09-29 16:59:00 -0300

    Fix spacing between cards

--- web/src/js/directives/Resize.js
+++ web/src/js/directives/Resize.js
@@ -19,19 +19,29 @@ tmj.directive('resize', function($window) {
                 if ($('.cards').masonry()) {
                     $('.cards').masonry('destroy');
                 }
-                $('.card').width(newValue.w - 100);
-                var m = ((newValue.w - ($('.card').width() + 20)) / 2) + 10;
+
+                $('.card').width(newValue.w - 80);
                 $('.card').eq(0).css({
-                    "margin-left": m
+                    "margin-left": 40
                 });
-                var e = $('.card').width() + 20
-                var w = e * $('.card').length;
+
                 $('.cards').css({
-                    "width": w + (m * 2) + 20,
-                    "max-width": w + (m * 2) + 20
+                    "width": 4000,
+                    "max-width": 4000,
+                    "margin": 0,
+                    "padding": 0
                 });
+
+                var e = $('.card').width() + 20
+                var w = (e * $('.card').length) + 70;
+                $('.cards').css({
+                    "width": w,
+                    "max-width": w
+                });
+
                 $('.card').attr('class', 'card');
-                var h = newValue.h - $('.card').height() + 51;
+                // -20 is the footer height
+                var h = newValue.h - $('header').height() - ($('.card').height() - $('.card .img').height()) - 20;
                 $('.card .img').height(h);
             }
         }, true);
