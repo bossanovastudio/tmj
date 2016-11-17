9011b827b4376ce897e3d181ad20ec3217afb351
Lucas Falcão Artacho <falcao.artacho@gmail.com>
2016-11-17 20:25:54 -0200

    frontend login page

--- web/src/js/controllers/LoginController.js
+++ web/src/js/controllers/LoginController.js
@@ -1,4 +1,9 @@
 tmj.controller('LoginController', function($rootScope, $scope, $http, $sce, $compile, $routeParams) {
+        
+    $('.initial-loading').hide(0);
+    $('.purple-overlay').show();
 
-
+    $scope.openPassword = function($event) {
+        $('.pass-box').addClass('show');
+    }
 });
