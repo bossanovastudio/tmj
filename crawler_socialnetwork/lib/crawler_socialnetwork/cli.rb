cfd3cdae5dc651e32bf61da027a314de06686ded
Adão Raul <adao.raul@gmail.com>
2016-11-17 17:45:54 -0200

    youtube crawler added

--- crawler_socialnetwork/lib/crawler_socialnetwork/cli.rb
+++ crawler_socialnetwork/lib/crawler_socialnetwork/cli.rb
@@ -3,12 +3,14 @@ module CrawlerSocialnetwork
     def initialize
       @facebook = CrawlerSocialnetwork::Facebook.new
       @twitter  = CrawlerSocialnetwork::Twitter.new
+      @youtube  = CrawlerSocialnetwork::Youtube.new
     end
 
     def run
       begin
         @twitter.tweets '#turmadamonicajovem'
         @facebook.timeline 'revistaturmadamonicajovem'
+        @youtube.channel 'UCekYMr9EQSauMefFzMTrpSg'
       rescue => e
         $logger.error(e)
       end
