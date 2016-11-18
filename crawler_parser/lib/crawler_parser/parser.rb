731b4173f75f4b8152c2ef6d707bf93b2b17db72
Adão Raul <adao.raul@gmail.com>
2016-11-18 15:56:59 -0200

    crawler/parser source_url bug fix

--- crawler_parser/lib/crawler_parser/parser.rb
+++ crawler_parser/lib/crawler_parser/parser.rb
@@ -15,11 +15,16 @@ module CrawlerParser
         unless @post.content.attachments.first.nil?
           @post.content.attachments.first['media']['image']['src'] if @post.content.attachments.first['type'] == 'photo'
         end
+      elsif @post.content.respond_to?('thumbnails')
+        unless @post.content.thumbnails['high'].nil?
+          @post.content.thumbnails['high']['url']
+        end
       end
     end
 
     def run
       image_id = self.send('image', self.image_url) unless self.image_url.nil?
+      p "social: #{@post.social_media}"
       card = self.send(@post.social_media, @post)
       
       unless image_id.nil?
@@ -37,7 +42,7 @@ module CrawlerParser
       card = Card.new
       card.origin     = :facebook
       card.content    = post.content.message
-      card.source_url = "https://facebook.com/" + post.id
+      card.source_url = "https://facebook.com/" + post.social_uuid unless post.social_uuid
       card.posted_at  = post.content.created_time
 
       card
@@ -47,8 +52,18 @@ module CrawlerParser
       card = Card.new
       card.origin     = :twitter
       card.content    = post.content.text
-      card.source_url = "https://twitter.com/statuses/" + post.id
+      card.source_url = "https://twitter.com/statuses/" + post.social_uuid unless post.social_uuid
       card.posted_at  = post.content.created_at
+
+      card
+    end
+    
+    def youtube(post)
+      card = Card.new
+      card.origin     = :youtube
+      card.content    = post.content.title
+      card.source_url = "https://www.youtube.com/watch?v=" + post.social_uuid unless post.social_uuid
+      card.posted_at  = post.content.publishedAt
 
       card
     end
