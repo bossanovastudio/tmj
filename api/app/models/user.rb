1eb2b56d27516e7ac2ad4fe6ffe48d2e34bac059
Adão Raul <adao.raul@gmail.com>
2016-11-24 16:57:18 -0200

    api editors setup

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ActiveRecord::Base
   # Include default devise modules.
-  devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
-  include DeviseTokenAuth::Concerns::User
+  devise :database_authenticatable, :registerable, :rememberable, :trackable, :omniauthable
+  # include DeviseTokenAuth::Concerns::User
   
   enum role: { user: 1, editor: 2, moderator: 3, admin: 4 }
   
+  # Uploader
+  mount_uploader :image, AvatarUploader
+  
   has_many :cards
   recommends :cards
+  
+  def email_required?
+    false
+  end
 end
---
+++ api/app/uploaders/avatar_uploader.rb
@@ -0,0 +1,21 @@
+class AvatarUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  storage :fog
+
+  def store_dir
+    "avatars/#{model.id}"
+  end
+
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  def default_url
+    # For Rails 3.1+ asset pipeline compatibility:
+    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+
+    image_path "avatar_default.png"
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+end
