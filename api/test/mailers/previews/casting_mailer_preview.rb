8c20c62daf95b75d9b7bc30a129226afd6e62fe5
Adão Raul <adao.raul@gmail.com>
2016-11-28 18:11:21 -0200

    mailer setup to mailgun

--- api/test/mailers/previews/casting_mailer_preview.rb
+++ api/test/mailers/previews/casting_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/casting_mailer
 class CastingMailerPreview < ActionMailer::Preview
-
+  def register_successful
+    CastingMailer.register_successful(Casting.first)
+  end
 end
