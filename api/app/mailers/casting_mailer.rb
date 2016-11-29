e6b079c251c39803c17048601c877f3b66a3cf25
Jonathan Querubina <jotadev@me.com>
2016-11-29 20:34:39 -0200

    Fix email subject

--- api/app/mailers/casting_mailer.rb
+++ api/app/mailers/casting_mailer.rb
@@ -1,8 +1,8 @@
 class CastingMailer < ApplicationMailer
   default from: 'casting@tmjofilme.com.br'
- 
+
     def register_successful(casting)
       @casting = casting
-      mail(to: @casting.adult_email, subject: 'Welcome to My Awesome Site')
+      mail(to: @casting.adult_email, subject: 'Inscrição realizada com sucesso.')
     end
 end
