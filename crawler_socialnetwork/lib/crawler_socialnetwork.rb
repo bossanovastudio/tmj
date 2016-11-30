12ef01425592b4f634cce4005b1f5d526fed790c
Victor Gama <hey@vito.io>
2016-11-30 16:39:22 -0200

    Implement Instagram crawler

--- crawler_socialnetwork/lib/crawler_socialnetwork.rb
+++ crawler_socialnetwork/lib/crawler_socialnetwork.rb
@@ -5,6 +5,7 @@ require "twitter"
 require "google/api_client"
 require "active_record"
 require "pinterest-api"
+require "httparty"
 
 require "crawler_socialnetwork/version"
 
@@ -12,6 +13,7 @@ require "crawler_socialnetwork/strategies/facebook"
 require "crawler_socialnetwork/strategies/twitter"
 require "crawler_socialnetwork/strategies/youtube"
 require "crawler_socialnetwork/strategies/pinterest"
+require "crawler_socialnetwork/strategies/instagram"
 
 require "crawler_socialnetwork/models/application_record"
 require "crawler_socialnetwork/models/crawled_post"
