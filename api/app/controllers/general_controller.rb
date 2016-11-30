25fa748446b512aca62935bee08daf16f255e349
Victor Gama <hey@vito.io>
2016-11-30 20:18:07 -0200

    Ensure cards order (Fix #91)

--- api/app/controllers/general_controller.rb
+++ api/app/controllers/general_controller.rb
@@ -1,20 +1,20 @@
 class GeneralController < ApplicationController
   def all
     pagination = pagination_params
-    
+
     @highlight = Highlight.page(pagination[:page]).per(1).first
-    @cards = Card.page(pagination[:page]).per(pagination[:quantity].to_i - 1).approved
+    @cards = Card.page(pagination[:page]).per(pagination[:quantity].to_i - 1).approved.ordered
   end
-  
+
   def highlights
     @highlights = Highlight.all
   end
-  
+
   def editors
     user = User.editors.where(username: params[:id]).first
     @cards = user.cards
   end
-  
+
   private
     def pagination_params
       params.permit(:page, :quantity)
