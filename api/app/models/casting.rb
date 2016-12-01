7de5ff282e60370fedd9a46507a8d583c50feac8
Adão Raul <adao.raul@gmail.com>
2016-12-01 02:04:24 -0200

    removing some validates

--- api/app/models/casting.rb
+++ api/app/models/casting.rb
@@ -59,7 +59,6 @@ class Casting < ApplicationRecord
   validates :adult_email, presence: true
   validates :adult_name, presence: true
   validates :adult_neighborhood, presence: true
-  validates :adult_phone, presence: true
   validates :adult_state, presence: true
   validates :adult_zip, presence: true
   validates :age, presence: true
@@ -68,21 +67,16 @@ class Casting < ApplicationRecord
   validates :document_rg, presence: true
   validates :eye_color, presence: true
   validates :hair_color, presence: true
-  validates :height, presence: true
   validates :instrument_description, presence: true, if: :instrument?
   validates :name, presence: true
-  validates :pants_size, presence: true
   validates :performance_description, presence: true, if: :performance?
   validates :photo_face, presence: true
   validates :photo_body, presence: true
   validates :photo_upperbody, presence: true
-  validates :shirt_size, presence: true
-  validates :shoe_size, presence: true
   validates :skin_color, presence: true
   validates :sport_description, presence: true, if: :sport?
   validates :theater_description, presence: true, if: :theater?
   validates :video, presence: true
-  validates :weight, presence: true
   
   # Uploader
   mount_uploader :photo_face, CastingUploader
