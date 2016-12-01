091e721c8c4496dd4edc0794132a4086f6cfde09
Jonathan Querubina <jotadev@me.com>
2016-12-01 10:41:05 -0200

    Validate CPF

--- web-register/src/js/controllers/RegisterController.js
+++ web-register/src/js/controllers/RegisterController.js
@@ -11,6 +11,26 @@ tmj.controller('RegisterController', function($rootScope, $location, $scope, $ht
     $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
     $('.form.active').css({ left: 0 });
 
+    $scope.validateCPF = function(strCPF) {
+        strCPF = strCPF.replace('.', '');
+        strCPF = strCPF.replace('.', '');
+        strCPF = strCPF.replace('-', '');
+        var sum;
+        var rest;
+        sum = 0;
+        if (strCPF == "00000000000") return false;
+        for (i = 1; i <= 9; i++) sum = sum + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+        rest = (sum * 10) % 11;
+        if ((rest == 10) || (rest == 11)) rest = 0;
+        if (rest != parseInt(strCPF.substring(9, 10))) return false;
+        sum = 0;
+        for (i = 1; i <= 10; i++) sum = sum + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+        rest = (sum * 10) % 11;
+        if ((rest == 10) || (rest == 11)) rest = 0;
+        if (rest != parseInt(strCPF.substring(10, 11))) return false;
+        return true;
+    }
+
     $scope.validForm = function() {
         var valid = true;
         $('.form.active').find('.field').each(function(i, f) {
@@ -21,6 +41,18 @@ tmj.controller('RegisterController', function($rootScope, $location, $scope, $ht
                 } else {
                     $(f).find('input, textarea, select').removeClass('error');
                 }
+
+                // CPF
+                if ($(f).find('#document_cpf, #adult_document_cpf').length > 0) {
+                    var field = $(f).find('#document_cpf, #adult_document_cpf');
+                    if (!$scope.validateCPF(field.val())) {
+                        field.addClass('error');
+                        valid = false;
+                    } else {
+                        field.removeClass('error');
+                    }
+                }
+
             } else if ($(f).hasClass('files')) {
                 $(f).find('input').each(function(i, f) {
                     if ($(f).val().length < 1) {
