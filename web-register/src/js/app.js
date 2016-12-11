6acc18156de8bff4f80235331a3a1eea09311c0f
Jonathan Querubina <jotadev@me.com>
2016-12-11 20:44:07 -0200

    Implement GA on Casting

--- web-register/src/js/app.js
+++ web-register/src/js/app.js
@@ -1,6 +1,20 @@
 var API_URL = "@@replace_api_grunt";
 var isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
+(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-88003802-1', 'auto');
+
 var tmj = angular.module('tmj', ['ngTouch', 'ngRoute', 'ngAnimate', 'ngCookies', 'ngResource', 'swipe']);
 tmj.config(function($routeProvider, $locationProvider, $httpProvider) {
         $routeProvider
@@ -32,4 +46,19 @@ tmj.config(function($routeProvider, $locationProvider, $httpProvider) {
                 $('.zip').mask('00000-000');
             }, 1);
         })
-    });
+    })
+    .run(['$route', '$rootScope', '$location', function($route, $rootScope, $location) {
+        ga('send', 'pageview', { page: $location.url() });
+        var original = $location.path;
+        $rootScope.previousURL = $location.$$path;
+        $location.path = function(path, reload) {
+            if (reload === false) {
+                var lastRoute = $route.current;
+                var un = $rootScope.$on('$locationChangeSuccess', function() {
+                    $route.current = lastRoute;
+                    un();
+                });
+            }
+            return original.apply($location, [path]);
+        };
+    }]);
