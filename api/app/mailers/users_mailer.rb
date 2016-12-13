d7779f782af91146852bd0b85cb4bcedfadc5d10
Junior Magalhães <juniormagalhaes2@gmail.com>
2016-12-13 17:36:54 -0200

    Add register and password emails

--- api/app/mailers/users_mailer.rb
+++ api/app/mailers/users_mailer.rb
@@ -1,6 +1,6 @@
 class UsersMailer < ApplicationMailer
     default from: '"TMJ" <noreply@tmjofilme.com.br>'
-
+    layout false
     def welcome(user)
       @user = user
       mail(to: @user.email, subject: 'Bem-vindo ao TMJ!')
--- api/test/mailers/previews/users_mailer_preview.rb
+++ api/test/mailers/previews/users_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/users_mailer
 class UsersMailerPreview < ActionMailer::Preview
-
+  def welcome
+    UsersMailer.welcome(User.first)
+  end
 end
