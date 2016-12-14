ef7f7259e54aa7b717ee7c364f08d43fc69bd254
Jonathan Querubina <jotadev@me.com>
2016-12-14 19:17:59 -0200

    Add or remove social neetworks

--- api/app/controllers/users/omniauth_callbacks_controller.rb
+++ api/app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,15 +1,15 @@
 class Users::OmniauthCallbacksController < ApplicationController
   def facebook
     omniauth_params = request.env["omniauth.auth"]
-    provider = Provider.from_omniauth omniauth_params
-    
+    provider = Provider.find_with_omniauth omniauth_params
+
     unless provider
-      Provider.create_with_omniauth omniauth_params
+      current_user.providers.create_with_omniauth omniauth_params
     end
-    
-    redirect_to '/sua-pagina'
+
+    redirect_to '/editar-perfil'
   end
-  
+
   def failure
     redirect_to root_path
   end
