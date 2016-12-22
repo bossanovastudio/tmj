0dc8e9d424b7c021bee2dee70f2189ed461f154e
Jonathan Querubina <jotadev@me.com>
2016-12-22 14:28:24 -0200

    Fix #323

--- api/app/assets/javascripts/site/directives/Directives.js
+++ api/app/assets/javascripts/site/directives/Directives.js
@@ -179,3 +179,14 @@ tmj.directive('fileOnChange', function() {
         }
     };
 });
+
+tmj.directive("ngMobileClick", [function () {
+    return function (scope, elem, attrs) {
+        elem.bind("touchstart click", function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            scope.$apply(attrs["ngMobileClick"]);
+        });
+    }
+}])
