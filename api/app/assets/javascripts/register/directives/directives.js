cacf302404d0afb96fd22c5e4d7e79611c36ec74
Jonathan Querubina <jotadev@me.com>
2016-12-26 15:09:57 -0200

    Update file names

---
+++ api/app/assets/javascripts/register/directives/directives.js
@@ -0,0 +1,9 @@
+tmj.directive('fileOnChange', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var onChangeHandler = scope.$eval(attrs.fileOnChange);
+            element.bind('change', onChangeHandler);
+        }
+    };
+});
