3693eccad669ddce0eb833d1a53b4dd01554ecc5
Jonathan Querubina <jotadev@me.com>
2016-12-27 10:49:56 -0200

    Revert "Update file names"
    This reverts commit cacf302404d0afb96fd22c5e4d7e79611c36ec74.

--- api/app/assets/javascripts/register/directives/directives.js
+++
@@ -1,9 +0,0 @@
-tmj.directive('fileOnChange', function() {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            var onChangeHandler = scope.$eval(attrs.fileOnChange);
-            element.bind('change', onChangeHandler);
-        }
-    };
-});
