7be42c56b6f15a88b7a2a7e44e8ca1b014f7d5d6
Jonathan Querubina <jotadev@me.com>
2016-12-27 13:41:33 -0200

    Update ramona create script

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,4 +1,4 @@
-User.create({ username: 'ramona', password: '123456', role: :editor, remote_image_url: 'https://s3-sa-east-1.amazonaws.com/cdntmjofilme/avatars/1/ramona.png' })
+User.create({ username: 'ramona', password: 'monmonmonmon', role: :editor, remote_image_url: 'https://s3-sa-east-1.amazonaws.com/cdntmjofilme/avatars/1/ramona.png' })
 Provider.create({ user: User.where(username: 'ramona').first, provider: 'pinterest', uid: '840484486616463777' })
 Provider.create({ user: User.where(username: 'ramona').first, provider: 'tumblr', uid: 'lunetalunatica' })
 Provider.create({ user: User.where(username: 'ramona').first, provider: 'youtube', uid: 'UC2sp_4csOUc4VnmNdWxTXhQ' })
