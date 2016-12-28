b058173a46aeea4ea59a5371cf7d157de5219e5f
Adão Raul <adao.raul@gmail.com>
2016-12-28 08:38:16 -0200

    set remix/details to public

--- api/app/controllers/remix_controller.rb
+++ api/app/controllers/remix_controller.rb
@@ -1,5 +1,5 @@
 class RemixController < ApplicationController
-  before_action :authenticate_user!, except: [:show]
+  before_action :authenticate_user!, only: [:index]
   layout 'remix'
 
   def index
