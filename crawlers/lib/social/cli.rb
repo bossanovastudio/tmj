dd1b94a0ca3a7a5f0663f93a14411227dfc2baf7
Victor Gama <hey@vito.io>
2016-12-29 19:43:32 -0200

    Crawlers: Remove root-level rescue block

--- crawlers/lib/social/cli.rb
+++ crawlers/lib/social/cli.rb
@@ -18,68 +18,64 @@ module Crawlers::Social
     end
 
     def run
-      begin
-        [
-          'TurmadaMonicaJovemBrasil',
-          'DoContradc',
-          'fasdeturmadamonicajovem',
-          'emersonabreu.msp',
-          'amamostmjforever',
-          'tmjdiversidades',
-          'OpiniaoTMJ',
-          'tmjmania',
-          'tmjjunto',
-          'TMJN0vos',
-          'tmjdivertidos',
-          'TMJ-CBM-Mania-1443398202598155',
-        ].each { |u| guard { @facebook.timeline u } }
+      [
+        'TurmadaMonicaJovemBrasil',
+        'DoContradc',
+        'fasdeturmadamonicajovem',
+        'emersonabreu.msp',
+        'amamostmjforever',
+        'tmjdiversidades',
+        'OpiniaoTMJ',
+        'tmjmania',
+        'tmjjunto',
+        'TMJN0vos',
+        'tmjdivertidos',
+        'TMJ-CBM-Mania-1443398202598155',
+      ].each { |u| guard { @facebook.timeline u } }
 
-        [
-          'OpiniaoTMJ',
-          'diariodorick',
-          'revistaturmajovem',
-        ].each { |u| guard { @youtube.channel u, true } }
+      [
+        'OpiniaoTMJ',
+        'diariodorick',
+        'revistaturmajovem',
+      ].each { |u| guard { @youtube.channel u, true } }
 
-        [
-          'UCekYMr9EQSauMefFzMTrpSg',
-          'UCpf5c40cP3MoXe2T81zb4xA',
-          'UCIfAeO5OrsuxczdozQ5mcag',
-        ].each { |u| guard { @youtube.channel u } }
+      [
+        'UCekYMr9EQSauMefFzMTrpSg',
+        'UCpf5c40cP3MoXe2T81zb4xA',
+        'UCIfAeO5OrsuxczdozQ5mcag',
+      ].each { |u| guard { @youtube.channel u } }
 
-        guard { @tumblr.search 'lunetalunatica.tumblr.com' }
-        guard { @pinterest.profile 'AWPUOpt6ygJugqlnzdUuNw07dfB-FIuTMID9YAtDl7PBEwAv_gAAAAA' }
+      guard { @tumblr.search 'lunetalunatica.tumblr.com' }
+      guard { @pinterest.profile 'AWPUOpt6ygJugqlnzdUuNw07dfB-FIuTMID9YAtDl7PBEwAv_gAAAAA' }
 
-        [
-          '_turmadamonica_',
-          'mundo_tmj15',
-          'tmj_turmadamonica',
-          'tmj_fanclub',
-          'monica_ironica',
-          'tmjprasempre',
-          'tmj_eterno',
-          'pride.tmj',
-          'forevercascali',
-          'monica_a_louca',
-          'denisetmj.oficial',
-          'cebola20',
-          'tmj_4050',
-        ].each { |u| guard { @instagram.profile u } }
+      [
+        '_turmadamonica_',
+        'mundo_tmj15',
+        'tmj_turmadamonica',
+        'tmj_fanclub',
+        'monica_ironica',
+        'tmjprasempre',
+        'tmj_eterno',
+        'pride.tmj',
+        'forevercascali',
+        'monica_a_louca',
+        'denisetmj.oficial',
+        'cebola20',
+        'tmj_4050',
+      ].each { |u| guard { @instagram.profile u } }
 
-        guard { @twitter.user 'revistadaturma' }
+      guard { @twitter.user 'revistadaturma' }
 
-        [
-          'turmadamonicaccxp',
-          'mspnaccxp',
-          'tmj100',
-          'turmadamonicajovem',
-          'tmjofilme',
-          'turmadamonicajovemofilme',
-        ].each do |hashtag|
-          guard { @instagram.hashtag hashtag }
-          guard { @twitter.search "##{hashtag}" }
-        end
-      rescue Exception => e
-        $logger.error(e)
+      [
+        'turmadamonicaccxp',
+        'mspnaccxp',
+        'tmj100',
+        'turmadamonicajovem',
+        'tmjofilme',
+        'turmadamonicajovemofilme',
+      ].each do |hashtag|
+        guard { @instagram.hashtag hashtag }
+        guard { @twitter.search "##{hashtag}" }
       end
     end
   end
