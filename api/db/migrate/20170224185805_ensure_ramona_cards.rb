2273e6f1cb29067c91e9bf7294b2d1ab79f7a638
Victor Gama <hey@vito.io>
2017-02-24 16:18:03 -0300

    Implement final migrations for Ramona and new moderation mechanism

---
+++ api/db/migrate/20170224185805_ensure_ramona_cards.rb
@@ -0,0 +1,14 @@
+class EnsureRamonaCards < ActiveRecord::Migration[5.0]
+  def up
+    u = User.find_by(username: 'ramona')
+    unless u.nil?
+      u.cards.each do |c|
+        c.moderate_for_editor(u.username, true)
+      end
+    end
+  end
+
+  def down
+    # Nothing to do! Yay!
+  end
+end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224175814) do
+ActiveRecord::Schema.define(version: 20170224190124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 20170224175814) do
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.string   "source_url"
-    t.integer  "status",              default: 1
     t.integer  "size",                default: 1
     t.json     "social_user"
     t.string   "social_uid"
